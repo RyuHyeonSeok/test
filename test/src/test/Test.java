13f51ca77cfbe5b1d9674d4ebde1c42c25984b62
RyuHyeonSeok <bangea78@gmail.com>
2016-01-09 01:11:48 +0900

    테스트 222

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		gksrmf xptmxm @@@@@@@@@@@@@@@@@@
+//		gksrmf xptmxm @@@@@@@@@@@@@@@@@@한글 등록 하기 힘드내
 	}
 
 }
